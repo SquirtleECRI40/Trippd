30bf38eed672dbcb93826fe2abd1f8bb16c9b53c
James Adler <james@echo-markets.com>
2023-06-26 19:35:27 -0400

    router create with sample components

--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
