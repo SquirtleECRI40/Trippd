680ceb70928e456c2594a6a2a405df35353b1c22
farrellkai <farrellkai@gmail.com>
2023-06-27 16:51:41 -0400

    get request trips

--- client/pages/DashboardPage.jsx
+++ client/pages/DashboardPage.jsx
@@ -1,13 +1,22 @@
 // displays all known trips
-
-import React, { useEffect } from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 function DashboardPage() {
   const { username } = useParams();
+  const [user, setUser] = useState(username);
+  const [trips, setTrips] = useState([]);
+
+  async function getData() {
+    const res = await axios.get(`/api/tripDashboard/${user}`);
+    const data = res.data;
+    setTrips(data);
+  }
 
   useEffect(() => {
     console.log('******THIS IS THE:', username);
+    getData();
   }, []);
 }
 export default DashboardPage;
