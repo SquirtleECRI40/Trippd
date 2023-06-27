6ad80114daa2b29314a14b181a840cb4018e346c
James Adler <james@echo-markets.com>
2023-06-27 11:18:40 -0400

    trip card (no modal, no styling

---
+++ client/public/components/TripCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const TripCard = (props) => {
+  //destructure props
+  //TO DO: do I need to get description for modal here?? or somewhere else
+  const { name, startLocation, endLocation, startDate, endDate } = props;
+
+  return (
+    <div className='trip-card'>
+      <h3>{name}</h3>
+      <h3>
+        {startLocation} to {endLocation}
+      </h3>
+      <h3>
+        {startDate} to {endDate}
+      </h3>
+      <button className = 'details-button'>More Details</button>
+    </div>
+  );
+};
