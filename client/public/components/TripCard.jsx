f17cdd29c278351d55741f220b2c85cdd5b8c344
James Adler <james@echo-markets.com>
2023-06-28 11:31:38 -0400

    updated put request and added correct labels
    Co-Authored-By: farrellkai <127214968+farrellkai@users.noreply.github.com>

--- client/public/components/TripCard.jsx
+++ client/public/components/TripCard.jsx
@@ -1,20 +1,19 @@
-import {React, useState} from 'react';
+import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Modal from 'react-modal';
 
 //import pages
 import TripModal from './pages/TripModal.jsx';
 
-
 Modal.setAppElement('#root');
 
-
 const TripCard = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   //destructure props
   //TO DO: do I need to get description for modal here?? or somewhere else
-  const { id, name, startLocation, endLocation, startDate, endDate, notes } = props;
+  const { _id, name, start_Location, location, start_Date, end_Date, notes } =
+    props;
 
   const openModal = () => {
     setIsOpen(true);
@@ -25,22 +24,35 @@ const TripCard = (props) => {
   };
 
   return (
-    <div className="trip-card">
+    <div className='trip-card'>
       <h3>{name}</h3>
       <h3>
-        {startLocation} to {endLocation}
+        {start_Location} to {location}
       </h3>
       <h3>
-        {startDate} to {endDate}
+        {start_Date} to {end_Date}
       </h3>
-      <button onClick={openModal} className = 'details-button'>More Details</button>
+      <button onClick={openModal} className='details-button'>
+        More Details
+      </button>
       <Modal
         isOpen={isOpen}
         onRequestClose={closeModal}
-        contentLabel="Example Modal"
+        contentLabel='Example Modal'
       >
-        <TripModal className = "trip-modal" closeModal = {closeModal} name = {name} startLocation = {startLocation} endLocation = {endLocation} startDate={startDate} endDate={endDate} notes={notes}/>
-      </Modal>    </div>
+        <TripModal
+          className='trip-modal'
+          _id = {_id}
+          closeModal={closeModal}
+          name={name}
+          startLocation={start_Location}
+          endLocation={location}
+          startDate={start_Date}
+          endDate={end_Date}
+          notes={notes}
+        />
+      </Modal>{' '}
+    </div>
   );
 };
 
