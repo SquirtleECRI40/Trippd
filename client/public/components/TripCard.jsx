892cff54dfa3b27acacbae466928e50de6525522
James Adler <james@echo-markets.com>
2023-06-28 11:05:20 -0400

    trip modal logic added

--- client/public/components/TripCard.jsx
+++ client/public/components/TripCard.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import {React, useState} from 'react';
 import { Link } from 'react-router-dom';
+import Modal from 'react-modal';
+
+//import pages
+import TripModal from './pages/TripModal.jsx';
+
+
+Modal.setAppElement('#root');
+
 
 const TripCard = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
   //destructure props
   //TO DO: do I need to get description for modal here?? or somewhere else
-  const { name, startLocation, endLocation, startDate, endDate } = props;
+  const { id, name, startLocation, endLocation, startDate, endDate, notes } = props;
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
 
   return (
     <div className='trip-card'>
@@ -15,7 +33,15 @@ const TripCard = (props) => {
       <h3>
         {startDate} to {endDate}
       </h3>
-      <button className = 'details-button'>More Details</button>
+      <button onClick={openModal} className = 'details-button'>More Details</button>
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+      >
+        <TripModal className = "trip-modal" closeModal = {closeModal} name = {name} startLocation = {startLocation} endLocation = {endLocation} startDate={startDate} endDate={endDate} notes={notes}/>
+      </Modal>
+    
     </div>
   );
 };
